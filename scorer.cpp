152ed884fb10b942db2af0e4e95c2745e106b6a7
Peter Chiang <peterchiang0119@gmail.com>
2022-03-31 19:39:08 +0800

    add comment to main function

--- scorer.cpp
+++ scorer.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <iostream>
 
 using namespace std;
@@ -23,13 +24,36 @@ int fourth_quarter()
 }
 int main()
 {
+    /* ----------Variable declaration---------- */
     int first_score, second_score, third_score, fourth_score, total_score;
+    string input_file_path = "input.txt";   // the given IC path
+    string output_file_path = "output.txt"; // silkscreen path
+
+    ifstream input_file, output_file;
+    /* ------End of Variable declaration------- */
+
+    /* -------------Reading files-------------- */
+
+    //!!!not finished!!!
+    input_file.open(input_file_path, ios::in);
+    output_file.open(output_file_path, ios::in);
+
+    /* ----------End of Reading Files---------- */
+
+    /* -----------Calling Functions------------ */
     first_score = first_quarter();
     second_score = second_quarter();
     third_score = third_quarter();
     fourth_score = fourth_quarter();
+    /* --------End of Calling Functions-------- */
 
     total_score = first_score + second_score + third_score + fourth_score;
 
+    /* --------------Score output-------------- */
+    cout << "first_score: " << first_score << endl
+         << "second_score: " << second_score << endl
+         << "third_score: " << third_score << endl
+         << "fourth_score: " << fourth_score << endl;
     cout << "The score of this silkscreen is: " << total_score << " / 100" << endl;
+    /* -----------End of Score output---------- */
 }
