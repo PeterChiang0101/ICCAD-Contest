442eff6b39d072bd2dfcb3ce64a8ac5308222dbb
Peter Chiang <peterchiang0119@gmail.com>
2022-04-29 23:10:13 +0800

    decided to use boost first

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+// use boost library
 
 using namespace std;
 
@@ -7,6 +8,11 @@ int main()
     // transform into data structure
 
     // calculate the silkscreen
+    // ignore the arc first
+    // maximize the usage of boost
+    // buffer() , closest_point(), distance(), within()
+
+    // arc needed to be treated manually
 
     // output
 }
