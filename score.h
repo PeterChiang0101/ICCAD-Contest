9a0915df282a943baf451b102508da3831e5184e
EggRollLin <raymond04281@gmail.com>
2022-07-04 21:08:34 +0800

    hello

---
+++ score.h
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef SEGMENT_H
+#define SEGMENT_H
+
+
+
+#endif
--- scorer.cpp
+++ scorer.cpp
@@ -5,6 +5,28 @@
 
 using namespace std;
 
+struct Segment
+{
+    bool is_line; // 0 = arc, 1 = line
+    float x1;
+    float y1;
+    float x2;
+    float y2;
+    float x_min, x_max, y_min, y_max; // the bounding box of the segment
+
+    // below are only used when is_line = 1
+    float slope;       //斜率
+    float y_intercept; //截距
+    double theta;      // the angle reference to positive x axis
+
+    // below are only used when is_line = 0
+    float center_x;
+    float center_y;
+    bool direction; // 0 = ClockWise(CW), 1 = ConterClockwise(CCW)
+    double theta_1; // 圓心到點一角度
+    double theta_2; // 圓心到點二角度
+};
+
 struct boarder
 {
     float x1{-100000};
