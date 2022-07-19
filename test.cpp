db908d0d0bd488060dcb1e6e300fc38c77787b12
PeterChiang0101 <peterchiang0119@gmail.com>
2022-07-19 15:54:10 +0800

    add limit to score, options to choose public or test

--- test.cpp
+++ test.cpp
@@ -6,7 +6,8 @@ using namespace std;
 
 int main()
 {
-     Scorer s("./PublicCase/PublicCase_C.txt", "./PublicCase/PublicCase_C_Ans.txt");
+     // Scorer s("./PublicCase/PublicCase_C.txt", "./PublicCase/PublicCase_C_Ans.txt");
+     Scorer s("./TestingCase/test_B.txt", "./TestingCase/test_B_Ans.txt");
      cout << setprecision(4) << fixed << s.Total_score() << endl;
      return 0;
 }
